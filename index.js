03f9df94ffbe22b3c20629caef86558fc3832565
Ujjwal Sharma <110032523+ujjwal5297@users.noreply.github.com>
2022-12-09 14:09:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('outputtext');
-let entryCount = 0;
-let entryLimit = 3;
-let error = false;
+// let entryCount = 0;
+// let entryLimit = 3;
+// let error = false;
 
-let number = [Math.floor(Math.random()*10)]
+Attempts = 3;
+
+let number = [Math.floor(Math.random()*10)];
+
+attemptID = document.getElementById('Attempts');
+attemptID.innerHTML = `Attempts left : ${Attempts}`;
+
+
+
 
 btn.addEventListener('click', function(){
     let input = document.getElementById('userInput').value;
@@ -22,14 +30,27 @@ btn.addEventListener('click', function(){
     else if (input != number){
         output.innerHTML= 'You guessed wrong number';
         document.body.style.backgroundColor = 'red';
-        for(let i= 0;i< entryLimit; i++) {
-            entryCount= i;
-            console.log(entryCount);
-            // while(entryCount=2){
-            //     document.getElementById('userOuptut').disabled = true;
-            //     output.innerHTML = 'You have reached the maximum attempts, you cannot guess more'
-            // }
+
+        Attempts = Attempts - 1;
+        console.log("Attempts left : "+Attempts);
+
+        attemptID.innerHTML = `Attempts left : ${Attempts}`;
+
+        if(Attempts==0)
+        {
+            document.getElementById("btn").disabled = true;
+            NoAttemptsLeftId= document.getElementById('NoAttemptsLeft');
+            NoAttemptsLeftId.innerHTML="Oops ! You ran out of attempts :("
         }
+
+        // for(let i= 0;i< entryLimit; i++) {
+        //     entryCount= i;
+        //     console.log(entryCount);
+        //     // while(entryCount=2){
+        //     //     document.getElementById('userOuptut').disabled = true;
+        //     //     output.innerHTML = 'You have reached the maximum attempts, you cannot guess more'
+        //     // }
+        // }
     };
     // if(input > number) {
     //     output.innerHTML= 'You guessed too high';
@@ -37,4 +58,4 @@ btn.addEventListener('click', function(){
     //     entryCount= entryCount++
     // };
 })
-console.log(entryCount)
+// console.log(entryCount)
