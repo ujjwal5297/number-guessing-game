b92deaaa61c352046e8471119df3288988d28b6e
Ujjwal Sharma <110032523+ujjwal5297@users.noreply.github.com>
2022-12-09 15:06:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('outputtext');
-// let entryCount = 0;
-// let entryLimit = 3;
-// let error = false;
 
-Attempts = 3;
+let Attempts = 3;
 
 let number = [Math.floor(Math.random()*10)];
 
 attemptID = document.getElementById('Attempts');
 attemptID.innerHTML = `Attempts left : ${Attempts}`;
 
+const startingseconds = 60;
+let time =startingseconds;
 
+const countDown = document.getElementById('timer');
+
+setInterval(updateCountDown(), 1000);
+
+function updateCountDown() {
+    countDown.innerHTML = `Time Remaining: ${startingseconds}`;
+    time--;
+}
 
 
 btn.addEventListener('click', function(){
@@ -20,13 +27,7 @@ btn.addEventListener('click', function(){
         output.innerHTML= `You guessed right number, your number was ${number}`;
         document.body.style.backgroundColor = 'green';
     }
-    // while(input != number) {
-    //     output.innerHTML= 'You guessed wrong number';
-    //     entryCount= entryCount++;}
 
-    //     while(entryCount = entryLimit) {
-    //         document.getElementById("userInput").disabled = true;
-    //     }
     else if (input != number){
         output.innerHTML= 'You guessed wrong number';
         document.body.style.backgroundColor = 'red';
@@ -36,26 +37,11 @@ btn.addEventListener('click', function(){
 
         attemptID.innerHTML = `Attempts left : ${Attempts}`;
 
-        if(Attempts==0)
+        if(Attempts==0 || time == 0)
         {
             document.getElementById("btn").disabled = true;
             NoAttemptsLeftId= document.getElementById('NoAttemptsLeft');
-            NoAttemptsLeftId.innerHTML="Oops ! You ran out of attempts :("
+            NoAttemptsLeftId.innerHTML= `Oops! you ran out of attempts or time, the correct number was ${number}`
         }
-
-        // for(let i= 0;i< entryLimit; i++) {
-        //     entryCount= i;
-        //     console.log(entryCount);
-        //     // while(entryCount=2){
-        //     //     document.getElementById('userOuptut').disabled = true;
-        //     //     output.innerHTML = 'You have reached the maximum attempts, you cannot guess more'
-        //     // }
-        // }
     };
-    // if(input > number) {
-    //     output.innerHTML= 'You guessed too high';
-    //     document.body.style.backgroundColor = 'red';
-    //     entryCount= entryCount++
-    // };
 })
-// console.log(entryCount)
