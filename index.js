1e79bcecdf9b04723bfc29d708d9be34c7154aec
Ujjwal Sharma <110032523+ujjwal5297@users.noreply.github.com>
2022-12-10 23:45:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,35 +3,60 @@ let output = document.getElementById('outputtext');
 
 let Attempts = 3;
 
-let number = [Math.ceil(Math.random()*10)];
+let number = 2;
+
+
+
+    var seconds = 60;
+    function tick() 
+    {
+      var counter = document.getElementById("counter");
+      seconds--;
+      counter.innerHTML ="0:" + (seconds < 10 ? "0" : "") + String(seconds);
+      if (seconds > 0) 
+      {
+        setTimeout(tick, 1000);
+      } 
+      else 
+      {
+        document.getElementById("btn").disabled = true;
+        document.getElementById("timeLeft").innerHTML="OOPS ! You ran out of time ! :("
+        document.getElementById("correctAnswer").innerHTML=`The correct guess was : ${number}`
+        
+      }
+    }
+    tick();
+
 
 attemptID = document.getElementById('Attempts');
 attemptID.innerHTML = `Attempts left : ${Attempts}`;
 
-let time = 60;
+// let time = 60;
 
-const countDown = document.getElementById('timer');
+// const countDown = document.getElementById('timer');
 
 
-function updateCountDown(x) {
+// function updateCountDown(x) {
     
-    countDown.innerHTML = `Time Remaining: ${time}`;
-    x-1;
-};
-setInterval(updateCountDown(time), 1000);
-console.log(updateCountDown(time));
+//     countDown.innerHTML = `Time Remaining: ${time}`;
+//     x-1;
+// };
+// setInterval(updateCountDown(time), 1000);
+// console.log(updateCountDown(time));
 
 
-
+let attempts = 0;
 
 btn.addEventListener('click', function(){
     let input = document.getElementById('userInput').value;
     if (input == number){
-        output.innerHTML= `You guessed right number, your number was ${number}`;
+      
+        output.innerHTML= `You guessed right number, your number was ${number}, you guess the right answer in ${attempts+1} attempts`;
         document.body.style.backgroundColor = 'green';
     }
 
     else if (input != number){
+      attempts++
         output.innerHTML= 'You guessed wrong number';
         document.body.style.backgroundColor = 'red';
 
@@ -40,7 +65,7 @@ btn.addEventListener('click', function(){
 
         attemptID.innerHTML = `Attempts left : ${Attempts}`;
 
-        if(Attempts==0 || time == 0)
+        if(Attempts== 0 )
         {
             document.getElementById("btn").disabled = true;
             NoAttemptsLeftId= document.getElementById('NoAttemptsLeft');
